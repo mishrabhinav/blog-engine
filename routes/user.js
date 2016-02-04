9e14ed98362283a0cd26fa724e632d2ad2ab8418
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-02-04 23:50:44 +0000

    Update routes/user.js, still not working.

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,6 @@
-
-/*
- * GET users listing.
- */
-
 exports.list = function(req, res){
   res.send('respond with a resource');
 };
-
-
-/*
- * GET login page.
- */
 
 exports.login = function(req, res, next) {
   res.render('login');
@@ -31,19 +21,10 @@ exports.add = function (req, res, next) {
   });
 };
 
-/*
- * GET logout route.
- */
-
 exports.logout = function(req, res, next) {
   req.session.destroy();
   res.redirect('/');
 };
-
-
-/*
- * POST authenticate route.
- */
 
 exports.authenticate = function(req, res, next) {
   if (!req.body.email || !req.body.password)
